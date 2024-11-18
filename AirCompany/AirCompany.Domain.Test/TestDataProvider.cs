eb648e378a36470b129874bdff79fc1629c73d84
Chuck-man <sgold2398@gmail.com>
2024-11-18 16:23:03 +0400

    Deleting lines

--- AirCompany/AirCompany.Domain.Test/TestDataProvider.cs
+++ AirCompany/AirCompany.Domain.Test/TestDataProvider.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using AirCompany.Domain;
-
-namespace AirCompany.Domain.Test;
+﻿namespace AirCompany.Domain.Test;
 
 public class TestDataProvider
 {
