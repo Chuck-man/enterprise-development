6d62147e95d90cdc84b7fb0427f9244e34e88898
Chuck-man <sgold2398@gmail.com>
2024-11-25 15:16:41 +0400

    add new project

---
+++ AirCompany/AirComany.API/Controllers/AircraftController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using AirCompany.Domain;
+
+namespace AirComany.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AircraftController : ControllerBase
+    {
+        // GET: api/<AircraftController>
+        [HttpGet]
+        public IEnumerable<Aircraft> Get()
+        {
+            return new List<Aircraft>;
+        }
+
+        // GET api/<AircraftController>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<AircraftController>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/<AircraftController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<AircraftController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
