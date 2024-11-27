a47ffef56d955d9ceb5e363760a69fe1b12fa015
Chuck-man <sgold2398@gmail.com>
2024-11-27 19:04:45 +0400

    creating services

---
+++ AirCompany/AirComany.API/Services/FlightService.cs
@@ -0,0 +1,51 @@
+﻿using AutoMapper;
+using AirCompany.Domain.Repositories;
+using AirCompany.API.Services;
+using AirCompany.Domain;
+using AirComany.API.DTO;
+
+namespace AirComany.API.Services;
+
+public class FlightService(
+    IRepository<Flight> flightRepository,
+    IRepository<Aircraft> aircraftRepository,
+    IRepository<RegisteredPassenger> registeredPassengerRepository,
+    IMapper mapper) : IService<FlightGetDto, FlightPostDto>
+{
+    private int _id = 1;
+
+    public bool Delete(int id)
+    {
+        return flightRepository.Delete(id);
+    }
+
+    public IEnumerable<FlightPostDto> GetAll()
+    {
+        return flightRepository.GetEntities().Select(mapper.Map<FlightPostDto>);
+    }
+
+    public FlightPostDto? GetById(int id)
+    {
+        var flight = flightRepository.GetId(id);
+        if (flight == null) return null;
+        var FlightPostDto = mapper.Map<FlightPostDto>(flight);
+        var test = registeredPassengerRepository.GetEntities().ToList();
+        FlightPostDto.Passengers = registeredPassengerRepository.GetEntities()
+            .Where(p => p.Flight?.Id == flight.Id)
+            .ToList()
+            .Select(mapper.Map<RegisteredPassengerPostDto>)
+            .ToList();
+        if (flight.PlaneType != null) FlightPostDto.PlaneTypeId = flight.PlaneType.Id;
+        return FlightPostDto;
+    }
+
+    public FlightPostDto? Post(FlightGetDto entity)
+    {
+        throw new NotImplementedException();
+    }
+
+    public bool Put(int id, FlightGetDto entity)
+    {
+        throw new NotImplementedException();
+    }
+}
---
+++ AirCompany/AirComany.API/Services/IService.cs
@@ -0,0 +1,44 @@
+﻿namespace AirCompany.API.Services;
+
+/// <summary>
+/// Интерфейс для сервисов сущностей
+/// </summary>
+/// <typeparam name="PDT">Тип сущности Dto</typeparam>
+/// <typeparam name="GDT">Тип сущности полная-Dto</typeparam>
+public interface IService<PDT, GDT>
+{
+    /// <summary>
+    /// Получение всех сущностей
+    /// </summary>
+    /// <returns>Список сущностей</returns>
+    public IEnumerable<GDT> GetAll();
+
+    /// <summary>
+    /// Получение сущности по идентификатору 
+    /// </summary>
+    /// <param name="id">Идентификатор сущности</param>
+    /// <returns>Сущность с выбранным идентификатором</returns>
+    public GDT? GetById(int id);
+
+    /// <summary>
+    /// Добавление сущности
+    /// </summary>
+    /// <param name="entity">Новая сущность</param>
+    /// <returns>Добавленная сущность</returns>
+    public GDT? Post(PDT entity);
+
+    /// <summary>
+    /// Обновляет сущность по идентификатору 
+    /// </summary>
+    /// <param name="id">Идентификатор сущности</param>
+    /// <param name="entity">Новая сущность</param>
+    /// <returns>Результат операции</returns>
+    public bool Put(int id, PDT entity);
+
+    /// <summary>
+    /// Удаляет сущность по идентификатору 
+    /// </summary>
+    /// <param name="id">Идентификатор сущности</param>
+    /// <returns>Результат операции</returns>
+    public bool Delete(int id);
+}
