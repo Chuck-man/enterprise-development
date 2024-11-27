8a67788ade86e83d9c0ff2fec38615bc90cb3f5c
Chuck-man <sgold2398@gmail.com>
2024-11-27 19:05:14 +0400

    changes

---
+++ AirCompany/AirComany.API/Mapping.cs
@@ -0,0 +1,21 @@
+﻿using AirCompany.Domain;
+using AirComany.API.DTO;
+using AutoMapper;
+
+namespace AirComany.API;
+
+public class Mapping : Profile
+{
+    public Mapping()
+    {
+        CreateMap<Aircraft, AircraftPostDto>();
+        CreateMap<Flight, AircraftPostDto>();
+        CreateMap<Passenger, PassengerPostDto>();
+        CreateMap<RegisteredPassenger, RegisteredPassengerPostDto>();
+
+        CreateMap<AircraftGetDto, Aircraft>().ReverseMap();
+        CreateMap<AircraftGetDto, Flight>().ReverseMap();
+        CreateMap<PassengerGetDto, Passenger>().ReverseMap();
+        CreateMap<RegisteredPassengerGetDto, RegisteredPassenger>().ReverseMap();
+    }
+}
